634888fb4f205faba3d81bc6703b068c16f95913
jtony2976 <jtony2976@hotmail.com>
2022-09-20 14:40:32 -0600

    tarea realizada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sum = 0;
 
+  for (let i = 0; i < num.length; i++) {
+     sum = sum + +num[i] * 2 ** (num.length - 1 - i);
+
+     //comentario: +num[i] -> significa que intenta convertir el valor de num[i] en un número, si aún no lo es.
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binary = (num % 2).toString();
 
+  while (num > 1) {
+      num = Math.floor(num / 2);
+      binary =  (num % 2) + (binary);
+  }
+
+  return binary;
 }
 
 
